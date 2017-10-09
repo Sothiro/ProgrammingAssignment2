f1689e69988dc599806e25ce43a1ebe06ae9162d
Sothiro <32643154+Sothiro@users.noreply.github.com>
2017-10-09 10:41:22 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Rprogramming homework 3 on Coursera
+## Sothiro pin
 
-## Write a short comment describing this function
+## makeCacheMatrix generates a matrix and stores its inverse if calculated using the
+## cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve will return the inverse of the matrix x, using the cached value if available (m is non null)
+## If not it will compute the inverse and store it using the setinv function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+
+## example with a random 5 by 5 matrix
+
+x=matrix(runif(100,-10,10),5,5)
+print("this is a random matrix:")
+print(x)
+z=makeCacheMatrix(x)
+cacheSolve(z)
+print("this is its inverse:")
+print(z$getinv())
